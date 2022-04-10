deed31dd4d0d825d10f8c9dd2bea7a2e7880c10d
Gregory Moraes <gregorymoraes1@hotmail.comgit config --global user.email gregorymoraes1@hotmail.comgit config --global user.email gregorymoraes1@hotmail.com>
2022-04-10 13:31:18 -0300

    :sparkles:Add seeds

---
+++ data/seeds/001_add_revendedores.js
@@ -0,0 +1,10 @@
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('revendedores').del()
+  await knex('revendedores').insert([
+    {nome: "Ford Satte Alam", contato: "3256.0014 - Paulo"},
+    {nome: "Chevrolet Uvel", contato: "3269.2998 - Janaina"},
+    {nome: "Fiat Felice", contato: "3215.2958 - Eduardo"},
+    {nome: "Volkswagen Panambra", contato: "3358.9467 - Fabricio"},
+    ]);
+};
