ec760ff6d502c6591a78a85af624db91dc8e2de2
Gregory Moraes <gregorymoraes1@hotmail.comgit config --global user.email gregorymoraes1@hotmail.comgit config --global user.email gregorymoraes1@hotmail.com>
2022-04-10 11:21:07 -0300

    :sparkles:Add Feature dev

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+module.exports = {
+  development: {
+    client: 'better-sqlite3',
+    connection: {
+      filename: './data/revenda.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: '.data/migrations'
+    },
+    seeds: {
+      directory: '.data/seeds'
+    }
+  }
+};
+
