d9e799820b90e0b63f896d22cf1f69c6a0ad32a7
Gregory Moraes <gregorymoraes1@hotmail.comgit config --global user.email gregorymoraes1@hotmail.comgit config --global user.email gregorymoraes1@hotmail.com>
2022-04-11 11:30:53 -0300

    :sparkles:Add full cars CRUD

--- carros.js
+++ carros.js
@@ -9,13 +9,58 @@ router.use(express.json());
 
 // método get é usado para consulta
 router.get("/", async (req, res) => {
-  try {
-    // para obter os carros pode-se utilizar .select().orderBy() ou apenas .orderBy()
-    const carros = await dbKnex("carros").orderBy("id", "modelo");
-    res.status(200).json(carros); // retorna statusCode ok e os dados
-  } catch (error) {
-    res.status(400).json({ msg: error.message }); // retorna status de erro e msg
-  }
+    try {
+        // para obter os carros pode-se utilizar .select().orderBy() ou apenas .orderBy()
+        const carros = await dbKnex("carros").orderBy("id", "modelo");
+        res.status(200).json(carros); // retorna statusCode ok e os dados
+    } catch (error) {
+        res.status(400).json({ msg: error.message }); // retorna status de erro e msg
+    }
+});
+
+
+// Método post é usado para inclusão
+router.post("/", async (req, res) => {
+    const { modelo, marca, ano, preco, revendedor_id } = req.body;
+
+    // se algum dos campos não foi passado, irá enviar uma mensagem de erro e retornar
+    if (!modelo || !marca || !ano || !preco || !revendedor_id) {
+        res.status(400).json({ msg: "Enviar modelo, marca, ano, preço e revendedor_id do veículo" });
+        return;
+    }
+
+    try {
+        // insert, faz a inserção na tabela carros (e retorna o id do registro inserido)
+        const novo = await dbKnex("carros").insert({ modelo, marca, ano, preco, revendedor_id });
+        res.status(201).json({ id: novo[0] }); // statusCode indica Create
+    } catch (error) {
+        res.status(400).json({ msg: error.message }); // retorna status de erro e msg
+    }
+});
+
+
+// Método put é usado para alteração. id indica o registro a ser alterado altera todos os campos
+router.put("/:id", async (req, res) => {
+    const id = req.params.id; // ou const { id } = req.params
+    const { modelo, marca, ano, preco, revendedor_id } = req.body; // campo a ser alterado
+    try {
+        await dbKnex("carros").update({ modelo, marca, ano, preco, revendedor_id }).where("id", id); // ou .where({ id })
+        res.status(200).json(); // statusCode indica Ok
+    } catch (error) {
+        res.status(400).json({ msg: error.message }); // retorna status de erro e msg
+    }
+});
+
+
+// Método delete é usado para exclusão
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params; // id do registro a ser excluído
+    try {
+        await dbKnex("carros").del().where({ id });
+        res.status(200).json(); // statusCode indica Ok
+    } catch (error) {
+        res.status(400).json({ msg: error.message }); // retorna status de erro e msg
+    }
 });
 
 
@@ -23,37 +68,13 @@ router.get("/", async (req, res) => {
 router.get("/pesquisar/:palavra", async (req, res) => {
     const { palavra } = req.params;
     try {
-      const carros = await dbKnex("carros").select("modelo", "marca", "ano", "preco")
-        .where("modelo", "like", `%${palavra}%`)
-        .orWhere("marca", "like", `%${palavra}%`)
-      res.status(200).json(carros); // retorna statusCode ok e os dados
+        const carros = await dbKnex("carros").select("modelo", "marca", "ano", "preco")
+            .where("modelo", "like", `%${palavra}%`)
+            .orWhere("marca", "like", `%${palavra}%`)
+        res.status(200).json(carros); // retorna statusCode ok e os dados
     } catch (error) {
-      res.status(400).json({ msg: error.message }); // retorna status de erro e msg
+        res.status(400).json({ msg: error.message }); // retorna status de erro e msg
     }
-  });
-
-
-
-
-
-
-  // Método post é usado para inclusão
-router.post("/", async (req, res) => {
-     const { modelo , marca, ano, preco, revendedor_id } = req.body;
-     
-     // se algum dos campos não foi passado, irá enviar uma mensagem de erro e retornar
-     if (!modelo || !marca || !ano || !preco || !revendedor_id) {
-    res.status(400).json({ msg: "Enviar modelo, marca, ano, preço e revendedor_id do veículo" });
-     return;
-     }
-     
-    try {
-    // insert, faz a inserção na tabela carros (e retorna o id do registro inserido)
-     const novo = await dbKnex("carros").insert({ modelo, marca, ano, preco, revendedor_id });
-     res.status(201).json({ id: novo[0] }); // statusCode indica Create
-     } catch (error) {
-     res.status(400).json({ msg: error.message }); // retorna status de erro e msg
-     }
-     });
+});
 
 module.exports = router;
