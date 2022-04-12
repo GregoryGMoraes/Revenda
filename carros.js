870e77d5a7c7a050312e4c07a04a9b512fc2cdb1
Gregory Moraes <gregorymoraes1@hotmail.comgit config --global user.email gregorymoraes1@hotmail.comgit config --global user.email gregorymoraes1@hotmail.com>
2022-04-12 10:41:45 -0300

    :sparkles:Update cars

--- carros.js
+++ carros.js
@@ -13,7 +13,6 @@ router.get("/", async (req, res) => {
         // para obter os carros pode-se utilizar .select().orderBy() ou apenas .orderBy()
         const carros = await dbKnex("carros")
         .orderBy("id", "modelo")
-        .innerJoin('revendedores', 'revendedor_id', 'revendedores.id');
         res.status(200).json(carros); // retorna statusCode ok e os dados
     } catch (error) {
         res.status(400).json({ msg: error.message }); // retorna status de erro e msg
